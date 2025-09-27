a229743168a1a5ce0c5743d1ebccb7a5cd9d35b0
Nuruddin Prathama <118452065+tamanpt@users.noreply.github.com>
2025-09-27 12:29:46 +0700

    Update procedure_ingestion_silver.sql

--- scripts/silver/procedure_ingestion_silver.sql
+++ scripts/silver/procedure_ingestion_silver.sql
@@ -159,7 +159,7 @@ BEGIN
 		PRINT '>> Inserting Data Into: silver.erp_loc_a101'
 		INSERT INTO [silver].[erp_loc_a101](cid,cntry)
 			SELECT
-			REPLACE(cid, '_', '') cid
+			REPLACE(cid, '-', '') cid
 			,CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
 				  WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
 				  WHEN TRIM(cntry) = '' or TRIM(cntry) IS NULL THEN '_NA'
